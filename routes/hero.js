dbe9024475e0123edc6fd47958eb12f0b8f8bd7e
saniya-abdul-rehman <46792211+saniya-abdul-rehman@users.noreply.github.com>
2019-09-14 00:10:43 +0500

    curd operation completed

--- routes/hero.js
+++ routes/hero.js
@@ -24,6 +24,30 @@ router.post('/heroes',(req,res)=>{
     });
 })
 
+//Hero delete
+
+router.delete('/heroes/:id', (req, res) => {
+    let id = req.params.id
+    conn.query(`DELETE FROM heroes Where id=${id}`, function (err, hero, fields) {
+      if (err)
+        res.json({ msg: err.message });;
+      res.json(hero)
+  
+    });
+  })
+
+//Hero update
+
+router.put('/heroes/:id', (req, res) => {
+    let id = req.params.id
+    const name =req.body.name;
+    conn.query(`UPDATE heroes SET name = '${name}' WHERE id = '${id}'`, function (err, hero) {
+      if (err)
+        res.json({ msg: err.message });;
+      res.json(hero)
+  
+    });
+  })
 
 module.exports=router;
 
